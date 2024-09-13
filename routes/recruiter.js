9747c9c0a3d067c7391aeafafb37f35bf20a10a2
Prasuna Dash <110610976+dprasuna@users.noreply.github.com>
2024-09-13 22:49:18 +0530

    updated

--- routes/recruiter.js
+++ routes/recruiter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Recruiter = require('../models/Recruiter');
+const Recruiter = require('../models/recruiter');
 const authMiddleware = require('../middleware/authMiddleware'); // Import auth middleware
 
 // POST /api/recruiters
